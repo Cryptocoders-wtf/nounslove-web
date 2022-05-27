1bacbce5e8b4a1f0c7b7c4fcb00021e0d1c529c7
isamu <isamu@to-kyo.to>
2022-05-27 17:59:09 +0900

    cleanup

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,7 @@ import Blank from "../components/Blank.vue";
 import NotFound from "../components/NotFound.vue";
 
 import Home from "../views/Home.vue";
-import Account from "../views/Account.vue";
 import About from "../views/About.vue";
-// import Test from "../views/Test.vue";
 
 const routeChildren: Array<RouteRecordRaw> = [
   {
@@ -17,14 +15,6 @@ const routeChildren: Array<RouteRecordRaw> = [
     path: "about",
     component: About,
   },
-  {
-    path: "account",
-    component: Account,
-  },
-  //  {
-  //    path: "test",
-  //    component: Test,
-  //  },
 ];
 
 const routes: Array<RouteRecordRaw> = [
